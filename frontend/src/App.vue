a83c9e903c2279249c646d054745844f11eb42f0
maxonclaxon <satamaks@gmail.com>
2021-03-24 11:59:00 +0300

    scss loader added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,6 +8,7 @@ import Auth from './components/Auth.vue'
 import Main from './components/Main.vue'
 import readCookie from './js/readcookie'
 
+
 export default {
   name: 'App',
   data(){
@@ -24,21 +25,11 @@ export default {
       console.log(readCookie('token'));
       if(readCookie('token')) return true;
       else return false;
-    }
+    },
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  width: 400px;
-  margin: 0 auto;
-  height: 100%;
-}
+<style lang='scss' scoped>
+
 </style>
