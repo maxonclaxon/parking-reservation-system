c3115bc864fb2d492f41cd89d0d8b971e3c7f13b
maxonclaxon <satamaks@gmail.com>
2021-03-29 10:52:41 +0300

    double parking on one place fixed

--- backend/bdfill.js
+++ backend/bdfill.js
@@ -3,6 +3,6 @@ var db = new sqlite3.Database('database.sqlite3')
 
 for(var floor = 0; floor<4; floor+=1){
     for(var place=0;place<20;place+=1){
-        db.run('INSERT INTO Space VALUES(null,?,?)',[floor+1,1])
+        db.run('INSERT INTO Space VALUES(null,?,?)',[floor+1,2])
     }
 }
